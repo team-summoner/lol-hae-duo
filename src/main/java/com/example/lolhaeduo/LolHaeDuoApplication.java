e1bf2edce35d335f76549f3de4b6f6fd9ec4963c
yokxim2 <yooksi53@gmail.com>
2024-12-03 09:38:45 +0900

    initial commit

---
+++ src/main/java/com/example/lolhaeduo/LolHaeDuoApplication.java
@@ -0,0 +1,13 @@
+package com.example.lolhaeduo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolHaeDuoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolHaeDuoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lolhaeduo/LolHaeDuoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lolhaeduo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolHaeDuoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
