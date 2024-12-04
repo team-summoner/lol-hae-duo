badcc9f6880a6a5bdf4831c4459870dd96062999
yokxim <yooksi53@gmail.com>
2024-12-04 18:38:14 +0900

    feat(puuid_response): ACCOUNT API의 응답값을 받는 Response DTO #3
    puuid, gameName, tagLine

---
+++ src/main/java/com/summoner/lolhaeduo/client/dto/PuuidResponse.java
@@ -0,0 +1,10 @@
+package com.summoner.lolhaeduo.client.dto;
+
+import lombok.Getter;
+
+@Getter
+public class PuuidResponse {
+    private String puuid;
+    private String gameName;
+    private String tagLine;
+}
