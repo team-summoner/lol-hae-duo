a24beb9f7be7c431c769487befee2441130ffead
yokxim <yooksi53@gmail.com>
2024-12-04 19:15:40 +0900

    chore(account_repository): AccountQuerydslRepository 추가 상속 #3

--- src/main/java/com/summoner/lolhaeduo/domain/account/repository/AccountRepository.java
+++ src/main/java/com/summoner/lolhaeduo/domain/account/repository/AccountRepository.java
@@ -3,5 +3,5 @@ package com.summoner.lolhaeduo.domain.account.repository;
 import com.summoner.lolhaeduo.domain.account.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccountRepository extends JpaRepository<Account, Long> {
+public interface AccountRepository extends JpaRepository<Account, Long>, AccountQuerydslRepository{
 }
