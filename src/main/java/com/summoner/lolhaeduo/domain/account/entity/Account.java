f3e4135c8c318028f3ce0013eb8dc340012acb3e
yokxim <yooksi53@gmail.com>
2024-12-04 18:24:14 +0900

    feat(account): 계정 등록 시 필요 정보 추가 #3
    태그라인, 지역, 서버 등등을 라이엇 API를 사용해서 정보를 가져올 예정

--- src/main/java/com/summoner/lolhaeduo/domain/account/entity/Account.java
+++ src/main/java/com/summoner/lolhaeduo/domain/account/entity/Account.java
@@ -1,18 +1,11 @@
 package com.summoner.lolhaeduo.domain.account.entity;
 
-import java.util.UUID;
-
 import com.summoner.lolhaeduo.common.entity.Timestamped;
+import com.summoner.lolhaeduo.domain.account.enums.AccountRegion;
+import com.summoner.lolhaeduo.domain.account.enums.AccountServer;
 import com.summoner.lolhaeduo.domain.account.enums.AccountType;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -40,17 +33,35 @@ public class Account extends Timestamped {
 	private String summonerName;
 
 	@Column(nullable = false)
+	private String tagLine;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private AccountRegion region;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private AccountServer server;
+
+	@Embedded
+	private AccountDetail accountDetail;
+
+	@Column(nullable = false)
 	private Long memberId;
 
-	private Account(String username, String password, AccountType accountType, String summonerName, Long memberId) {
+	private Account(String username, String password, AccountType accountType, String summonerName, String tagLine, AccountServer server, AccountDetail accountDetail, Long memberId) {
 		this.username = username;
 		this.password = password;
 		this.accountType = accountType;
 		this.summonerName = summonerName;
+		this.tagLine = tagLine;
+		this.server = server;
+		this.region = server.getRegion();
+		this.accountDetail = accountDetail;
 		this.memberId = memberId;
 	}
 
-	public static Account of(String username, String password, AccountType accountType, String summonerName, Long memberId) {
-		return new Account(username, password, accountType, summonerName, memberId);
+	public static Account of(String username, String password, AccountType accountType, String summonerName, String tagLine, AccountServer server, AccountDetail accountDetail, Long memberId) {
+		return new Account(username, password, accountType, summonerName, tagLine, server, accountDetail, memberId);
 	}
 }
