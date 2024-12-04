4681dc1b89db9a742c81f0405277b70b691314e6
yokxim <yooksi53@gmail.com>
2024-12-04 18:32:44 +0900

    feat(account_service): 계정 연동 API #3
    계정을 연동하는 메서드 linkAccount 구현
    ACCOUNT / SUMMONER API를 호출하여 response 값을
    PuuidResponse, SummonerResponse 형태로 가져와서
    필요한 값을 Account의 AccountDetail 값으로 저장한다.

--- src/main/java/com/summoner/lolhaeduo/domain/account/service/AccountService.java
+++ src/main/java/com/summoner/lolhaeduo/domain/account/service/AccountService.java
@@ -1,7 +1,65 @@
 package com.summoner.lolhaeduo.domain.account.service;
 
+import com.summoner.lolhaeduo.client.dto.PuuidResponse;
+import com.summoner.lolhaeduo.client.dto.SummonerResponse;
+import com.summoner.lolhaeduo.client.riot.RiotUtil;
+import com.summoner.lolhaeduo.domain.account.dto.LinkAccountRequest;
+import com.summoner.lolhaeduo.domain.account.entity.Account;
+import com.summoner.lolhaeduo.domain.account.entity.AccountDetail;
+import com.summoner.lolhaeduo.domain.account.enums.AccountType;
+import com.summoner.lolhaeduo.domain.account.repository.AccountRepository;
+import com.summoner.lolhaeduo.domain.member.entity.Member;
+import com.summoner.lolhaeduo.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AccountService {
+
+    private final MemberRepository memberRepository;
+    private final AccountRepository accountRepository;
+    private final RiotUtil riotUtil;
+
+    /**
+     * link account (for now, members can only link with RIOT account)
+     * @param request accountType, accountId, accountPassword, summonerName, tagLine, server
+     * @param memberId from token
+     */
+    public void linkAccount(LinkAccountRequest request, Long memberId) {
+        memberRepository.findById(memberId).orElseThrow(() -> new IllegalArgumentException("Not Found Member"));
+
+        if (request.getAccountType().equals(AccountType.RIOT)) {
+
+            PuuidResponse puuidResponse = riotUtil.extractPuuid(
+                    request.getSummonerName(),
+                    request.getTagLine(),
+                    request.getServer().getRegion()
+            );
+
+            SummonerResponse summonerResponse = riotUtil.extractSummonerInfo(
+                    puuidResponse.getPuuid(),
+                    request.getServer()
+            );
+
+            AccountDetail newAccountDetail = AccountDetail.of(
+                    puuidResponse.getPuuid(),
+                    summonerResponse.getAccountId(),
+                    summonerResponse.getId()
+            );
+
+            Account newAccount = Account.of(
+                    request.getAccountId(),
+                    request.getAccountPassword(),
+                    AccountType.RIOT,
+                    request.getSummonerName(),
+                    request.getTagLine(),
+                    request.getServer(),
+                    newAccountDetail,
+                    memberId
+            );
+
+            accountRepository.save(newAccount);
+        }
+    }
 }
