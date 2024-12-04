5b3462d1798a058f4054e4a48fab1f895ed160d1
yokxim <yooksi53@gmail.com>
2024-12-04 18:30:06 +0900

    feat(account_repository): 계정 repository 클래스 #3
    JPARepository를 활용

--- src/main/java/com/summoner/lolhaeduo/domain/account/repository/AccountRepository.java
+++ src/main/java/com/summoner/lolhaeduo/domain/account/repository/AccountRepository.java
@@ -1,4 +1,7 @@
 package com.summoner.lolhaeduo.domain.account.repository;
 
-public class AccountRepository {
+import com.summoner.lolhaeduo.domain.account.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
 }
