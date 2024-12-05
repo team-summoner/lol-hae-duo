fdd746d8972b04eb58be0761d4471c639e91a4db
eun0 <shinely12@gmail.com>
2024-12-05 09:30:08 +0900

    feat(auth-filter): 필터 생성 #7
    - MemberController 생성
      - 회원 가입 메서드 signup
      - 로그인 메서드 login

--- src/main/java/com/summoner/lolhaeduo/domain/member/controller/MemberController.java
+++ src/main/java/com/summoner/lolhaeduo/domain/member/controller/MemberController.java
@@ -1,4 +1,38 @@
 package com.summoner.lolhaeduo.domain.member.controller;
 
+import com.summoner.lolhaeduo.domain.member.dto.LoginRequest;
+import com.summoner.lolhaeduo.domain.member.dto.LoginResponse;
+import com.summoner.lolhaeduo.domain.member.dto.SignupRequest;
+import com.summoner.lolhaeduo.domain.member.service.MemberService;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+@AllArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+    
+    @PostMapping("/signup")
+    public ResponseEntity<Void> signup(@RequestBody SignupRequest signupRequest) {
+        memberService.signup(signupRequest);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest, HttpServletResponse response) {
+        LoginResponse loginResponse = memberService.login(loginRequest, response);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(loginResponse);
+    }
 }
