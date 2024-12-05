65e63efdddbc644f46ee6ae7bc6470226e0556b6
yokxim <yooksi53@gmail.com>
2024-12-05 10:30:01 +0900

    feat(account-repository): 중복확인 로직 추가
    계정 ID가 unique value이므로 중복확인 로직을 추가함

--- src/main/java/com/summoner/lolhaeduo/domain/account/repository/AccountRepository.java
+++ src/main/java/com/summoner/lolhaeduo/domain/account/repository/AccountRepository.java
@@ -4,4 +4,5 @@ import com.summoner.lolhaeduo.domain.account.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long>, AccountQuerydslRepository{
+    boolean existsByUsername(String username);
 }
