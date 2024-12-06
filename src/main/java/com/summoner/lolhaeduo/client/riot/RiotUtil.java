a0bb7014befd2a9ba96157d80cbc312813c54fec
hubaek <zjfl3122@naver.com>
2024-12-06 13:03:21 +0900

    feat(RiotUtil): leagueV4 api 값 가져오기 #8
    - 리그정보가 담긴 API 호출

--- src/main/java/com/summoner/lolhaeduo/client/riot/RiotUtil.java
+++ src/main/java/com/summoner/lolhaeduo/client/riot/RiotUtil.java
@@ -1,5 +1,6 @@
 package com.summoner.lolhaeduo.client.riot;
 
+import com.summoner.lolhaeduo.client.dto.LeagueEntryResponse;
 import com.summoner.lolhaeduo.client.dto.PuuidResponse;
 import com.summoner.lolhaeduo.client.dto.SummonerResponse;
 import com.summoner.lolhaeduo.domain.account.enums.AccountRegion;
@@ -56,4 +57,16 @@ public class RiotUtil {
         // Execute request
         return restTemplate.getForObject(url, SummonerResponse.class);
     }
+
+    public LeagueEntryResponse extractLeagueInfo(String summonerId, AccountServer server) {
+        String serverDomain = server.name().toLowerCase();
+
+        String url = String.format(
+                "https://%s.api.riotgames.com/lol/league/v4/entries/by-summoner/%s?api_key=%s",
+                serverDomain, summonerId, apiKey
+        );
+
+        return restTemplate.getForObject(url, LeagueEntryResponse.class);
+    }
+
 }
