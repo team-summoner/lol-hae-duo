849d0de1bd4485089282c686851850ef78f17653
hubaek <zjfl3122@naver.com>
2024-12-07 23:44:49 +0900

    fix(QueueType): RiotAPI 반환값에 따른 Enum 값 변경 #21
    - SOLO, FLEX 일경우 넘겨 받는 값이 정해져 있어서 해당 값으로 로직 변경

--- src/main/java/com/summoner/lolhaeduo/domain/duo/enums/QueueType.java
+++ src/main/java/com/summoner/lolhaeduo/domain/duo/enums/QueueType.java
@@ -1,5 +1,28 @@
 package com.summoner.lolhaeduo.domain.duo.enums;
 
 public enum QueueType {
-	QUICK, SOLO, FLEX
-}
+	// QUICK 은 어떤 값으로 넘겨주는 찾아야함
+	QUICK("QUICK"),
+	SOLO("RANKED_SOLO_5x5"),
+	FLEX("RANKED_FLEX_SR");
+
+	private final String riotQueueType;
+
+	QueueType(String riotQueueType) {
+		this.riotQueueType = riotQueueType;
+	}
+
+	public String getRiotQueueType() {
+		return riotQueueType;
+	}
+
+	public static QueueType fromRiotQueueType(String riotQueueType) {
+		for (QueueType type : values()) {
+			if (type.riotQueueType.equalsIgnoreCase(riotQueueType)) {
+				return type;
+			}
+		}
+			throw new IllegalArgumentException("Unknown QueueType: " + riotQueueType);
+		}
+	}
+
