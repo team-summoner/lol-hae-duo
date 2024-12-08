b7ae42bb33de7f5a3c59e8480423952b3ae9c8aa
hubaek <zjfl3122@naver.com>
2024-12-09 00:49:24 +0900

    feat(DuoController): duo 등록 추가 #21
    - duo 등록 API 추가

--- src/main/java/com/summoner/lolhaeduo/domain/duo/controller/DuoController.java
+++ src/main/java/com/summoner/lolhaeduo/domain/duo/controller/DuoController.java
@@ -1,4 +1,29 @@
 package com.summoner.lolhaeduo.domain.duo.controller;
 
+import com.summoner.lolhaeduo.common.annotation.Auth;
+import com.summoner.lolhaeduo.common.dto.AuthMember;
+import com.summoner.lolhaeduo.domain.duo.dto.DuoCreateRequest;
+import com.summoner.lolhaeduo.domain.duo.dto.DuoCreateResponse;
+import com.summoner.lolhaeduo.domain.duo.service.DuoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class DuoController {
+
+    private final DuoService duoService;
+
+    @PostMapping("/duo")
+    public ResponseEntity<DuoCreateResponse> createDuo(@RequestBody DuoCreateRequest createRequest, @Auth AuthMember authMember) {
+
+        DuoCreateResponse duoCreateResponse = duoService.createDuo(createRequest,authMember.getMemberId());
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(duoCreateResponse);
+    }
 }
