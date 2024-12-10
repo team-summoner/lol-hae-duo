51af363fc4aa884bddf88df0026ee0df9dbd82f2
yokxim <yooksi53@gmail.com>
2024-12-10 15:40:45 +0900

    fix(matchStats) 매치 정보에서 승리, 패배 횟수 카운트

--- src/main/java/com/summoner/lolhaeduo/client/dto/MatchStats.java
+++ src/main/java/com/summoner/lolhaeduo/client/dto/MatchStats.java
@@ -1,5 +1,6 @@
 package com.summoner.lolhaeduo.client.dto;
 
+import com.summoner.lolhaeduo.domain.duo.enums.QueueType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -7,9 +8,13 @@ import lombok.Getter;
 @AllArgsConstructor
 public class MatchStats {
 
+    private final int wins;
+    private final int losses;
+    private final int totalGames;
+    private final QueueType queueType;
+
     private final double winRate;
     private final double averageKill;
     private final double averageDeath;
     private final double averageAssist;
-    private final int totalGames;
 }
