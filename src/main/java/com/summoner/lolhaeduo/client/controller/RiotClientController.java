c25d6b35122e2536095f0892a2ba7c921755ba70
yokxim <yooksi53@gmail.com>
2024-12-10 11:43:20 +0900

    feat(riot-client-controller): 버전 최신화 업데이트 메서드 수동 호출용 컨트롤러

---
+++ src/main/java/com/summoner/lolhaeduo/client/controller/RiotClientController.java
@@ -0,0 +1,19 @@
+package com.summoner.lolhaeduo.client.controller;
+
+import com.summoner.lolhaeduo.client.riot.DataDragonScheduler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class RiotClientController {
+    private final DataDragonScheduler dataDragonScheduler;
+
+    @PostMapping("/ddragon")
+    public ResponseEntity<Void> updateDataDragonInfo() {
+        dataDragonScheduler.updateDataDragonInfo();
+        return ResponseEntity.ok().build();
+    }
+}
