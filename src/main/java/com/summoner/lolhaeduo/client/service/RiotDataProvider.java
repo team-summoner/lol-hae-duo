e941cf92b28fb2d9e8ef64838b183d3f3f3c2250
yokxim <yooksi53@gmail.com>
2024-12-11 20:11:29 +0900

    chore(riot-data-provider) getProfileIconUrl 메서드 삭제 #30
    인터페이스로서 공유되지 않을 메서드라고 판단해서 지웠습니다.

--- src/main/java/com/summoner/lolhaeduo/client/service/RiotDataProvider.java
+++ src/main/java/com/summoner/lolhaeduo/client/service/RiotDataProvider.java
@@ -2,7 +2,6 @@ package com.summoner.lolhaeduo.client.service;
 
 import com.summoner.lolhaeduo.client.dto.MatchStats;
 import com.summoner.lolhaeduo.client.dto.RankStats;
-import com.summoner.lolhaeduo.domain.account.entity.Account;
 import com.summoner.lolhaeduo.domain.account.enums.AccountRegion;
 import com.summoner.lolhaeduo.domain.account.enums.AccountServer;
 import com.summoner.lolhaeduo.domain.duo.enums.QueueType;
@@ -10,8 +9,7 @@ import com.summoner.lolhaeduo.domain.duo.enums.QueueType;
 import java.util.List;
 
 public interface RiotDataProvider {
-    RankStats getRankGameStats(Account account, AccountServer server);
+    RankStats getRankGameStats(String summonerId, AccountServer server);
     List<String> getMatchIds(QueueType queueType, int playCount, AccountRegion region, String puuid);
     MatchStats getMatchStats(Long accountId, List<String> matchIds, QueueType queueType, String summonerName, String tagLine, AccountRegion region);
-    String getProfileIconUrl(String puuid, AccountServer server);
 }
