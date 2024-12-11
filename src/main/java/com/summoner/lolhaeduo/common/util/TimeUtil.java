480bd2d4356b08ba432c90fe63d33402668eb0f0
yokxim <yooksi53@gmail.com>
2024-12-11 10:20:51 +0900

    feat(time-util) convertToEpochSeconds 메서드 #30
    - LocalDateTime을 입력값으로 줄 때 epoch 단위시간을 반환하는 메서드입니다.
    - AccountGameData는 업데이트한 날짜를 LocalDateTime 타입으로 같이 저장하므로, 해당 날짜부터 현재 날짜까지 RiotClient의 getMatchIds 메서드를 호출하기 쉽게 LocalDateTime 시간값을 변환하는데 도움을 줄 메서드입니다.
    - 이 메서드가 불필요해 보인다면 꼭 의견 주십시오.

--- src/main/java/com/summoner/lolhaeduo/common/util/TimeUtil.java
+++ src/main/java/com/summoner/lolhaeduo/common/util/TimeUtil.java
@@ -3,14 +3,23 @@ package com.summoner.lolhaeduo.common.util;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.time.temporal.ChronoUnit;
 
 @Component
 public class TimeUtil {
 
-    public long startTimeInEpoch(int days) {
+    public static long startTimeInEpoch(int days) {
         Instant now = Instant.now();
         Instant past = now.minus(days, ChronoUnit.DAYS);
         return past.getEpochSecond();
+    }
+
+    public static long convertToEpochSeconds(LocalDateTime dateTime) {
+        if (dateTime == null) {
+            throw new IllegalArgumentException("dateTime is null");
+        }
+        return dateTime.toEpochSecond(ZoneOffset.UTC);
     }
 }
