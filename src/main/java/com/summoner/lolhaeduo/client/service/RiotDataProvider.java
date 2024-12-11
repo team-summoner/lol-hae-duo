e5c87705350800840f2bf51fa5a4a1c3fefc36dc
yokxim <yooksi53@gmail.com>
2024-12-11 10:21:45 +0900

    feat(riot-data-provider) RiotClientService 부모 인터페이스 #30
    객체 상호 간 의존성을 낮추기 위해 만들어 본 인터페이스입니다.

---
+++ src/main/java/com/summoner/lolhaeduo/client/service/RiotDataProvider.java
@@ -0,0 +1,17 @@
+package com.summoner.lolhaeduo.client.service;
+
+import com.summoner.lolhaeduo.client.dto.MatchStats;
+import com.summoner.lolhaeduo.client.dto.RankStats;
+import com.summoner.lolhaeduo.domain.account.entity.Account;
+import com.summoner.lolhaeduo.domain.account.enums.AccountRegion;
+import com.summoner.lolhaeduo.domain.account.enums.AccountServer;
+import com.summoner.lolhaeduo.domain.duo.enums.QueueType;
+
+import java.util.List;
+
+public interface RiotDataProvider {
+    RankStats getRankGameStats(Account account, AccountServer server);
+    List<String> getMatchIds(QueueType queueType, int playCount, AccountRegion region, String puuid);
+    MatchStats getMatchStats(Long accountId, List<String> matchIds, QueueType queueType, String summonerName, String tagLine, AccountRegion region);
+    String getProfileIconUrl(Account linkedAccount);
+}
