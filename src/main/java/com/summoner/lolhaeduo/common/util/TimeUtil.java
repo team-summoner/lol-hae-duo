114b130fa7b2027398275ed10afdb2ebb18ef5bc
yokxim <yooksi53@gmail.com>
2024-12-11 20:12:32 +0900

    chore(time-util) startTimeInEpoch 삭제 #30
    몇일 전 시간을 인수 값으로 넣는 것보다, LocalDateTime 타입 값을 인수로 받는 것이 더 확장성이 높다고 판단하여 수정했습니다.

--- src/main/java/com/summoner/lolhaeduo/common/util/TimeUtil.java
+++ src/main/java/com/summoner/lolhaeduo/common/util/TimeUtil.java
@@ -2,21 +2,12 @@ package com.summoner.lolhaeduo.common.util;
 
 import org.springframework.stereotype.Component;
 
-import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.time.temporal.ChronoUnit;
 
 @Component
 public class TimeUtil {
-
-    public static long startTimeInEpoch(int days) {
-        Instant now = Instant.now();
-        Instant past = now.minus(days, ChronoUnit.DAYS);
-        return past.getEpochSecond();
-    }
-
-    public static long convertToEpochSeconds(LocalDateTime dateTime) {
+    public long convertToEpochSeconds(LocalDateTime dateTime) {
         if (dateTime == null) {
             throw new IllegalArgumentException("dateTime is null");
         }
