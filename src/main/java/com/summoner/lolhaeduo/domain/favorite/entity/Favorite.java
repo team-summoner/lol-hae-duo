4204b2a3cf5454159980dfbea707d45d577208e3
yokxim <yooksi53@gmail.com>
2024-12-19 09:46:38 +0900

    rename(favorite) 문서 위치 수정 client -> domain #37

--- src/main/java/com/summoner/lolhaeduo/domain/favorite/entity/Favorite.java
+++ src/main/java/com/summoner/lolhaeduo/domain/favorite/entity/Favorite.java
@@ -1,4 +1,4 @@
-package com.summoner.lolhaeduo.client.entity;
+package com.summoner.lolhaeduo.domain.favorite.entity;
 
 import com.summoner.lolhaeduo.domain.duo.enums.QueueType;
 import jakarta.persistence.*;
@@ -31,12 +31,16 @@ public class Favorite {
     @Column(nullable = false)
     private int winCount = 0;
 
-    public Favorite(Long accountId, QueueType queueType, String key, int playCount, int winCount) {
+    private Favorite(Long accountId, QueueType queueType, String key, int playCount, int winCount) {
         this.accountId = accountId;
         this.queueType = queueType;
         this.championName = key;
         this.playCount = playCount;
         this.winCount = winCount;
+    }
+
+    public static Favorite of(Long accountId, QueueType queueType, String key, int playCount, int winCount) {
+        return new Favorite(accountId, queueType, key, playCount, winCount);
     }
 
     public void update(int playCount, int winCount) {
