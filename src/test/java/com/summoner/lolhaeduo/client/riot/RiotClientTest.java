6b131d952ef67f950ffd5aae2338c318a4640213
yokxim <yooksi53@gmail.com>
2024-12-19 09:48:03 +0900

    rename(riot-client-service-test) dto 개편으로 인한 import 변화 #37

--- src/test/java/com/summoner/lolhaeduo/client/riot/RiotClientTest.java
+++ src/test/java/com/summoner/lolhaeduo/client/riot/RiotClientTest.java
@@ -1,9 +1,5 @@
 package com.summoner.lolhaeduo.client.riot;
 
-import com.summoner.lolhaeduo.client.dto.FormattedMatchResponse;
-import com.summoner.lolhaeduo.client.dto.LeagueEntryResponse;
-import com.summoner.lolhaeduo.client.dto.PuuidResponse;
-import com.summoner.lolhaeduo.client.dto.SummonerResponse;
 import com.summoner.lolhaeduo.domain.account.enums.AccountRegion;
 import com.summoner.lolhaeduo.domain.account.enums.AccountServer;
 import org.junit.jupiter.api.Assertions;
