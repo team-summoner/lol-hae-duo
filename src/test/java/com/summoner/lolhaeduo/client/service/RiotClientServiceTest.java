8bc79a04bc2c9f5205203d6de60746290b8bb92b
yokxim <yooksi53@gmail.com>
2024-12-19 09:47:39 +0900

    rename(riot-client-service-test) 패키지 위치 수정에 따른 import 변화 #37

--- src/test/java/com/summoner/lolhaeduo/client/service/RiotClientServiceTest.java
+++ src/test/java/com/summoner/lolhaeduo/client/service/RiotClientServiceTest.java
@@ -1,10 +1,10 @@
 package com.summoner.lolhaeduo.client.service;
 
-import com.summoner.lolhaeduo.client.repository.FavoriteRepository;
 import com.summoner.lolhaeduo.client.repository.VersionRepository;
 import com.summoner.lolhaeduo.client.riot.RiotClient;
 import com.summoner.lolhaeduo.common.util.TimeUtil;
 import com.summoner.lolhaeduo.domain.account.entity.Account;
+import com.summoner.lolhaeduo.domain.favorite.repository.FavoriteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
