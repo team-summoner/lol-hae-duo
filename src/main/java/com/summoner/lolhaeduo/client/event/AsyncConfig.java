997ca29b4e963ce53b2bb58dbe2b69873398697e
yokxim <yooksi53@gmail.com>
2024-12-22 17:39:42 +0900

    feat(async-config) 스레드 풀 조정 #44
    - 주석을 참고해주세요.

--- src/main/java/com/summoner/lolhaeduo/client/event/AsyncConfig.java
+++ src/main/java/com/summoner/lolhaeduo/client/event/AsyncConfig.java
@@ -1,9 +1,26 @@
 package com.summoner.lolhaeduo.client.event;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @Configuration
 @EnableAsync
-public class AsyncConfig {
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);                // Thread pool에서 기본적으로 유지되는 스레드의 개수
+        executor.setMaxPoolSize(10);                // Thread pool에서 사용할 수 있는 최대 스레드의 개수
+        executor.setQueueCapacity(100);             // 작업 큐가 최대로 가질 수 있는 작업 개수
+        executor.setThreadNamePrefix("custom-");    // 생성되는 스레드 이름 커스텀
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.AbortPolicy()); // 작업 큐가 가득 찬 상태에서 작업이 추가될 때 발생하는 예외
+        executor.initialize();
+        return executor;
+    }
 }
