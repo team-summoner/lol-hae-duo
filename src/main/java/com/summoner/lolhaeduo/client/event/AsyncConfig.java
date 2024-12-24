7f6ca423540563ee25635cd7fdde1a73bc0c39c5
yokxim <yooksi53@gmail.com>
2024-12-24 10:09:47 +0900

    fix(async-config) config 설정값 변경 #44
    t2 micro 서버를 사용하는 상황에서 CPU 코어 수는 1개이므로 나온 계산입니다.
    corePoolSize = CPU 코어 수 * 2
    maxPoolSize = corePoolSize * 2 ~ 3
    queueCapacity = 비동기로 호출되는 작업 수
    
    queueCapacity의 경우 일반적인 duo.gg 사이트에서 이용하는 사람들의 평균 수인 150명을 커버할 수 있는 숫자로 대체했습니다.

--- src/main/java/com/summoner/lolhaeduo/client/event/AsyncConfig.java
+++ src/main/java/com/summoner/lolhaeduo/client/event/AsyncConfig.java
@@ -15,9 +15,9 @@ public class AsyncConfig implements AsyncConfigurer {
     @Override
     public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(5);                // Thread pool에서 기본적으로 유지되는 스레드의 개수
-        executor.setMaxPoolSize(10);                // Thread pool에서 사용할 수 있는 최대 스레드의 개수
-        executor.setQueueCapacity(100);             // 작업 큐가 최대로 가질 수 있는 작업 개수
+        executor.setCorePoolSize(2);                // Thread pool에서 기본적으로 유지되는 스레드의 개수
+        executor.setMaxPoolSize(5);                // Thread pool에서 사용할 수 있는 최대 스레드의 개수
+        executor.setQueueCapacity(200);             // 작업 큐가 최대로 가질 수 있는 작업 개수
         executor.setThreadNamePrefix("custom-");    // 생성되는 스레드 이름 커스텀
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.AbortPolicy()); // 작업 큐가 가득 찬 상태에서 작업이 추가될 때 발생하는 예외
         executor.initialize();
