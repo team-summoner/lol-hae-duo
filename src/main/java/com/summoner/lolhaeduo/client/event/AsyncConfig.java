730120037d78ac26b7a90f52595fc252da88867f
eun0 <shinely12@gmail.com>
2024-12-25 21:38:23 +0900

    fix(async-config) thread-pool 설정값 변경 #44
    - CorePoolSize 측정 공식에 따른 안정적인 CorePoolSize 값 설정
    - MaxPoolSize 값 설정 삭제
      - QueueCapacity 값 설정 삭제에 따른 MaxPoolSize 값 설정 의미가 없어짐

--- src/main/java/com/summoner/lolhaeduo/client/event/AsyncConfig.java
+++ src/main/java/com/summoner/lolhaeduo/client/event/AsyncConfig.java
@@ -1,5 +1,6 @@
 package com.summoner.lolhaeduo.client.event;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -8,6 +9,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadPoolExecutor;
 
+@Slf4j
 @Configuration
 @EnableAsync
 public class AsyncConfig implements AsyncConfigurer {
@@ -15,9 +17,12 @@ public class AsyncConfig implements AsyncConfigurer {
     @Override
     public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(2);                // Thread pool에서 기본적으로 유지되는 스레드의 개수
-        executor.setMaxPoolSize(5);                // Thread pool에서 사용할 수 있는 최대 스레드의 개수
-        executor.setThreadNamePrefix("custom-");    // 생성되는 스레드 이름 커스텀
+
+        int cpuCoreCount = Runtime.getRuntime().availableProcessors();      // 서버의 CPU 코어 수 확인
+//        log.info("[확인] cpuCoreCount = {}", cpuCoreCount);
+
+        executor.setCorePoolSize(cpuCoreCount * 2);                         // Thread Pool 에서 기본적으로 유지되는 스레드의 개수
+        executor.setThreadNamePrefix("custom-");                            // 생성되는 스레드 이름 커스텀
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.AbortPolicy()); // 작업 큐가 가득 찬 상태에서 작업이 추가될 때 발생하는 예외
         executor.initialize();
         return executor;
