d9ee3f043e6b1d97abd9f93525935233a43ad7ba
yokxim <yooksi53@gmail.com>
2024-12-26 12:13:37 +0900

    delete(profileIcon-repository) 외부 API DB로 넘어간 entity의 repository #47

--- src/main/java/com/summoner/lolhaeduo/client/repository/ProfileIconRepository.java
+++
@@ -1,8 +0,0 @@
-package com.summoner.lolhaeduo.client.repository;
-
-import com.summoner.lolhaeduo.client.entity.Champion;
-import com.summoner.lolhaeduo.client.entity.ProfileIcon;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileIconRepository extends JpaRepository<ProfileIcon, Long> {
-}
