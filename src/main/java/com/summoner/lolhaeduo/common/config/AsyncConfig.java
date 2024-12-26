3d6aefc550569146c5270a78398525aa09152cf6
yokxim <yooksi53@gmail.com>
2024-12-26 12:01:22 +0900

    rename(async-config) 패키지 경로 수정 client/config -> common/config #47

--- src/main/java/com/summoner/lolhaeduo/common/config/AsyncConfig.java
+++ src/main/java/com/summoner/lolhaeduo/common/config/AsyncConfig.java
@@ -1,4 +1,4 @@
-package com.summoner.lolhaeduo.client.event;
+package com.summoner.lolhaeduo.common.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
