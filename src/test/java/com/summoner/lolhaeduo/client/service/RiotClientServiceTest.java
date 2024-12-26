6a69c2d7e34ff51fc2009fa0a74c286ba19f3bcf
yokxim <yooksi53@gmail.com>
2024-12-26 12:08:53 +0900

    rename(riot-client-service-test) 테스트파일 주석처리 #47

--- src/test/java/com/summoner/lolhaeduo/client/service/RiotClientServiceTest.java
+++ src/test/java/com/summoner/lolhaeduo/client/service/RiotClientServiceTest.java
@@ -1,34 +1,28 @@
 package com.summoner.lolhaeduo.client.service;
 
-import com.summoner.lolhaeduo.client.repository.FavoriteRepository;
-import com.summoner.lolhaeduo.client.repository.VersionRepository;
-import com.summoner.lolhaeduo.client.riot.RiotClient;
-import com.summoner.lolhaeduo.common.util.TimeUtil;
-import com.summoner.lolhaeduo.domain.account.entity.Account;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @Transactional
 class RiotClientServiceTest {
-
-    @Autowired
-    private RiotClientService riotClientService;
-
-    @Autowired
-    private RiotClient riotClient;
-
-    @Autowired
-    private FavoriteRepository favoriteRepository;
-
-    @Autowired
-    private VersionRepository versionRepository;
-
-    @Autowired
-    private TimeUtil timeUtil;
-
-    private Account account;
+//
+//    @Autowired
+//    private RiotClientService riotClientService;
+//
+//    @Autowired
+//    private RiotClient riotClient;
+//
+//    @Autowired
+//    private FavoriteRepository favoriteRepository;
+//
+//    @Autowired
+//    private VersionRepository versionRepository;
+//
+//    @Autowired
+//    private TimeUtil timeUtil;
+//
+//    private Account account;
 
 
 }
