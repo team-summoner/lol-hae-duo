720712bfe00d92b94e437c09c5e62baa4bd9483a
yokxim <yooksi53@gmail.com>
2024-12-26 11:02:22 +0900

    fix(riot-client-util) restTemplate 호출 메서드 exchange 획일화 #37
    범용성이 높은 exchange 메서드로 모든 호출을 일관성 있게 수정했습니다.

--- src/main/java/com/summoner/lolhaeduo/client/riot/util/RiotClientUtil.java
+++ src/main/java/com/summoner/lolhaeduo/client/riot/util/RiotClientUtil.java
@@ -7,6 +7,7 @@ import com.summoner.lolhaeduo.client.riot.dto.response.RiotApiRankInfoResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -19,61 +20,97 @@ import java.util.List;
 public class RiotClientUtil {
 
     private final RestTemplate restTemplate;
-    private final String riotClientApiBaseUrl = "http://52.79.242.133:8080/riot-client";
+    private final String riotClientApiBaseUrl = "http://localhost:8081/riot-client";
+
+    private HttpHeaders headers = new HttpHeaders();
 
     // RiotClientController - /account-infos [POST]
     public RiotApiAccountInfoResponse getAccountInfos(RiotApiAccountInfoRequest request) {
         String url = riotClientApiBaseUrl + "/account-infos";
-        ResponseEntity<RiotApiAccountInfoResponse> response = restTemplate.postForEntity(
-                url, request, RiotApiAccountInfoResponse.class
+
+        headers.set("Content-Type", "application/json");
+
+        HttpEntity<RiotApiAccountInfoRequest> requestEntity = new HttpEntity<>(request, headers);
+
+        ResponseEntity<RiotApiAccountInfoResponse> response = restTemplate.exchange(
+                url, HttpMethod.POST, requestEntity, RiotApiAccountInfoResponse.class
         );
+
         return response.getBody();
     }
 
     // RiotClientController - /update-profile-icon [POST]
     public String getProfileIcon(RiotApiUpdateProfileRequest request) {
         String url = riotClientApiBaseUrl + "/update-profile-icon";
-        ResponseEntity<String> response = restTemplate.postForEntity(
-                url, request, String.class
+
+        headers.set("Content-Type", "application/json");
+
+        HttpEntity<RiotApiUpdateProfileRequest> requestEntity = new HttpEntity<>(request, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                url, HttpMethod.POST, requestEntity, String.class
         );
+
         return response.getBody();
     }
 
     // RiotClientController - /rank-infos [POST]
     public RiotApiRankInfoResponse getRankInfos(RiotApiRankInfoRequest request) {
         String url = riotClientApiBaseUrl + "/rank-infos";
-        ResponseEntity<RiotApiRankInfoResponse> response = restTemplate.postForEntity(
-                url, request, RiotApiRankInfoResponse.class
+
+        headers.set("Content-Type", "application/json");
+
+        HttpEntity<RiotApiRankInfoRequest> requestEntity = new HttpEntity<>(request, headers);
+
+        ResponseEntity<RiotApiRankInfoResponse> response = restTemplate.exchange(
+                url, HttpMethod.POST, requestEntity, RiotApiRankInfoResponse.class
         );
+
         return response.getBody();
     }
 
     // RiotClientController - /match-ids [POST]
     public List<String> getMatchIds(RiotApiMatchIdRequest request) {
         String url = riotClientApiBaseUrl + "/match-ids";
-        HttpEntity<RiotApiMatchIdRequest> requestEntity = new HttpEntity<>(request);
+
+        headers.set("Content-Type", "application/json");
+
+        HttpEntity<RiotApiMatchIdRequest> requestEntity = new HttpEntity<>(request, headers);
+
         ResponseEntity<List<String>> response = restTemplate.exchange(
                 url, HttpMethod.POST, requestEntity, new ParameterizedTypeReference<>() {}
         );
+
         return response.getBody();
     }
 
     // RiotClientController - /match-ids [PUT]
     public List<String> updateMatchIds(RiotApiUpdateMatchIdRequest request) {
         String url = riotClientApiBaseUrl + "/match-ids";
-        HttpEntity<RiotApiUpdateMatchIdRequest> requestEntity = new HttpEntity<>(request);
+
+        headers.set("Content-Type", "application/json");
+
+        HttpEntity<RiotApiUpdateMatchIdRequest> requestEntity = new HttpEntity<>(request, headers);
+
         ResponseEntity<List<String>> response = restTemplate.exchange(
                 url, HttpMethod.PUT, requestEntity, new ParameterizedTypeReference<>() {}
         );
+
         return response.getBody();
     }
 
     // RiotClientController - /match-infos [POST]
     public RiotApiMatchInfoResponse getMatchInfos(RiotApiMatchInfoRequest request) {
         String url = riotClientApiBaseUrl + "/match-infos";
-        ResponseEntity<RiotApiMatchInfoResponse> response = restTemplate.postForEntity(
-                url, request, RiotApiMatchInfoResponse.class
+
+        headers.set("Content-Type", "application/json");
+
+        HttpEntity<RiotApiMatchInfoRequest> requestEntity = new HttpEntity<>(request, headers);
+
+        ResponseEntity<RiotApiMatchInfoResponse> response = restTemplate.exchange(
+                url, HttpMethod.POST, requestEntity, RiotApiMatchInfoResponse.class
         );
+
         return response.getBody();
     }
 
