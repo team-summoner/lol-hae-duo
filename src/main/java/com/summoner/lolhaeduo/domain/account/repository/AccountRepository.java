dcc411a10971da1d9be90c0f73663c31940a4f46
yokxim <yooksi53@gmail.com>
2024-12-29 22:48:41 +0900

    feat(account-repository) SummonerName과 TagLine으로 Account 찾는 로직 추가

--- src/main/java/com/summoner/lolhaeduo/domain/account/repository/AccountRepository.java
+++ src/main/java/com/summoner/lolhaeduo/domain/account/repository/AccountRepository.java
@@ -3,7 +3,10 @@ package com.summoner.lolhaeduo.domain.account.repository;
 import com.summoner.lolhaeduo.domain.account.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account, Long>, AccountQuerydslRepository{
     boolean existsByUsername(String username);
     boolean existsBySummonerNameAndTagLine(String summonerName, String summonerName1);
+    Optional<Account> findBySummonerNameAndTagLine(String summonerName, String summonerName1);
 }
