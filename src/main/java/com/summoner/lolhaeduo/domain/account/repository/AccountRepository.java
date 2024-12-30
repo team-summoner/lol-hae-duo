612f37b924f04ef780a55719747600bf64a1b12d
yokxim <yooksi53@gmail.com>
2024-12-30 09:54:37 +0900

    rename(account-repository) 오타 수정 summonerName1 -> tagLine

--- src/main/java/com/summoner/lolhaeduo/domain/account/repository/AccountRepository.java
+++ src/main/java/com/summoner/lolhaeduo/domain/account/repository/AccountRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 public interface AccountRepository extends JpaRepository<Account, Long>, AccountQuerydslRepository{
     boolean existsByUsername(String username);
     boolean existsBySummonerNameAndTagLine(String summonerName, String summonerName1);
-    Optional<Account> findBySummonerNameAndTagLine(String summonerName, String summonerName1);
+    Optional<Account> findBySummonerNameAndTagLine(String summonerName, String tagLine);
 }
