89e2a6922a148ac412007b1a49f2c4681f85acb0
yokxim <yooksi53@gmail.com>
2024-12-30 10:37:16 +0900

    delete(account-repository) 사용하지 않는 메서드 삭제

--- src/main/java/com/summoner/lolhaeduo/domain/account/repository/AccountRepository.java
+++ src/main/java/com/summoner/lolhaeduo/domain/account/repository/AccountRepository.java
@@ -3,10 +3,7 @@ package com.summoner.lolhaeduo.domain.account.repository;
 import com.summoner.lolhaeduo.domain.account.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface AccountRepository extends JpaRepository<Account, Long>, AccountQuerydslRepository{
     boolean existsByUsername(String username);
     boolean existsBySummonerNameAndTagLine(String summonerName, String tagLine);
-    Optional<Account> findBySummonerNameAndTagLine(String summonerName, String tagLine);
 }
