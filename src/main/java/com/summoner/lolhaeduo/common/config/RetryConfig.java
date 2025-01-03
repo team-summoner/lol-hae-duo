b954f769b929335eb281fcf1f52a27a16076f2f0
yokxim <yooksi53@gmail.com>
2025-01-03 14:23:42 +0900

    feat(retry-config) Spring Retry를 사용하기 위한 Configuration 클래스

---
+++ src/main/java/com/summoner/lolhaeduo/common/config/RetryConfig.java
@@ -0,0 +1,9 @@
+package com.summoner.lolhaeduo.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@EnableRetry
+@Configuration
+public class RetryConfig {
+}
